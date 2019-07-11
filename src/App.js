6533cc2c71acebd5094546e2910ee63126fdb5d6
Rich Frost <richard@ipponsolutions.co.uk>
2019-07-11 17:28:24 +0100

    Updated UI to render image uploader along with results

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, HashRouter as Router } from "react-router-dom";
+import ImageUpload from "./screens/image-upload/index";
+import Results from "./screens/results";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <main>
+          <div className="section" id="index-banner">
+            <div className="container">
+              <div className="row">
+                <div className="col s12 m12 xl12">
+                  <Route path="/" component={ImageUpload} />
+                  <Route path="/results" component={Results} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </main>
+      </div>
+    </Router>
   );
 }
 
